940e10986971e9f64240eab5bddfbb532e2b7139
Michael Spitsin <spitsinmichael@gmail.com>
2020-01-04 10:52:21 +0400

    #11: added doc-reason, why MainThreadExecutor is public

--- library/src/main/java/com/programmerr47/phroom/MainThreadExecutor.kt
+++ library/src/main/java/com/programmerr47/phroom/MainThreadExecutor.kt
@@ -4,6 +4,9 @@ import android.os.Handler
 import android.os.Looper
 import java.util.concurrent.Executor
 
+//This class should be internal, since it actually created for the purposes of library
+//But Sample module currently needs MainThreadExecutor for PagingLibrary and
+//I didn't want to duplicate it. So I've made it public :(
 class MainThreadExecutor : Executor {
     private val uiHandler = Handler(Looper.getMainLooper())
 
