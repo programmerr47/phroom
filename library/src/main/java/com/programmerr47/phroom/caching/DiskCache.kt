05ffcea27b7f0c9551b27fe6b54d4f40d58ab367
Mikhail Spitsin <spitsinmichael@gmail.com>
2020-01-13 01:17:31 +0400

    #5: added limitation for disk cache

--- library/src/main/java/com/programmerr47/phroom/caching/DiskCache.kt
+++ library/src/main/java/com/programmerr47/phroom/caching/DiskCache.kt
@@ -2,6 +2,8 @@ package com.programmerr47.phroom.caching
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import androidx.collection.LruCache
+import com.programmerr47.phroom.atLeastKitkat
 import com.programmerr47.phroom.kutils.createSafe
 import com.programmerr47.phroom.kutils.deleteSafe
 import java.io.File
@@ -12,39 +14,85 @@ import kotlin.concurrent.read
 import kotlin.concurrent.write
 
 internal class DiskCache(
-    private val cacheDir: File
+    private val cacheDir: File,
+    private val maxBytesCount: Int
 ) : BitmapCache {
     private val _lock = ReentrantReadWriteLock()
 
+    private val records = object : LruCache<String, Record>(maxBytesCount) {
+        override fun sizeOf(key: String, value: Record) = value.size
+
+        override fun entryRemoved(evicted: Boolean, key: String, oldValue: Record, newValue: Record?) {
+            tryToRemove(key)
+        }
+    }
+
     init {
         require(cacheDir.isDirectory)
+        parseJournal()
     }
 
     override fun get(key: String): Bitmap? = _lock.read {
+        val key = key.md5()
         val cached = getFile(key)
         cached?.let { BitmapFactory.decodeStream(FileInputStream(it)) }
     }
 
     override fun put(key: String, bitmap: Bitmap): Unit = _lock.write {
+        val key = key.md5()
         tryToRemove(key)
 
-        val bitmapFile = File(cacheDir, key.md5())
+        val bitmapFile = File(cacheDir, key)
         if (bitmapFile.createSafe()) {
             runCatching { bitmapFile.writePng(bitmap) }
+                .onSuccess { records.put(key, Record(key, with(bitmap) { if (atLeastKitkat()) allocationByteCount else byteCount })) }
                 .onFailure { tryToRemove(key) }
         }
+
+        saveJournal()
     }
 
     private fun tryToRemove(key: String) {
         getFile(key)?.deleteSafe()
+        records.remove(key)
     }
 
     private fun getFile(key: String): File? {
-        val desiredName = key.md5()
-        return getCachedFiles().find { it.name == desiredName }
+        val possible = File(cacheDir, key)
+        return possible.takeIf { it.exists() } ?: getCachedFiles().find { it.name == key }
     }
 
     private fun getCachedFiles() = cacheDir.listFiles() ?: emptyArray()
+
+    private fun parseJournal() {
+        getJournalFile().reader().buffered().useLines {
+            it.forEach {
+                val record = parseLine(it)
+                records.put(record.name, record)
+            }
+        }
+    }
+
+    private fun parseLine(line: String): Record {
+        val parts = line.split(" ")
+        return Record(parts[0], parts[1].toInt())
+    }
+
+    //TODO Now we save journal on every single update, which looks like time spending. Need to optimize that
+    private fun saveJournal() {
+        getJournalFile().bufferedWriter().use {
+            records.snapshot().forEach { (_, record) ->
+                it.write(record.toFileStr())
+                it.newLine()
+            }
+        }
+    }
+
+    private fun getJournalFile() = File(cacheDir, JOUNRAL_FILE).apply {
+        if (!exists()) {
+            createNewFile()
+        }
+    }
 
     private fun String.md5() =
         MessageDigest.getInstance("MD5").digest(toByteArray()).toHex()
@@ -53,4 +101,15 @@ internal class DiskCache(
 
     private fun File.writePng(bitmap: Bitmap) =
         outputStream().use { bitmap.compress(Bitmap.CompressFormat.PNG, 0, it) }
+
+    private class Record(
+        val name: String,
+        val size: Int //in bytes
+    ) {
+        fun toFileStr() = "$name $size"
+    }
+
+    companion object {
+        private const val JOUNRAL_FILE = ".journal"
+    }
 }
